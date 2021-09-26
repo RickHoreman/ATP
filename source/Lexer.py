e6a4853897128c3b86dda20841fdd7b1d4ae143e
Rick Horeman <rickhoreman@gmail.com>
2021-09-26 16:36:44 +0200

    Added support for variable definitions, booleans, functions defintions, and started adding more honorifics

--- source/Lexer.py
+++ source/Lexer.py
@@ -9,25 +9,37 @@ def readFile(inputFilePath : str) -> str:
     with open(inputFilePath, "r", encoding="utf8") as inputFile:
         return inputFile.read() 
 
-# lexNext :: String -> Int -> Int -> List[Token] -> String -> Int -> List[Token]
-def lexInt(input : str, lineNr : int, charNr : int, tokens : List[Tokens.Token], number : str='', value : int=0) -> List[Tokens.Token]:
+# listStartsWith :: List[String] -> List[String] -> Bool
+def listStartsWith(list1 : List[str], list2 : List[str]) -> bool:
+    if len(list2) <= 0:
+        return True
+    elif len(list1) <= 0:
+        return False
+    char1, *rest1 = list1
+    char2, *rest2 = list2
+    if char1 != char2:
+        return False
+    else:
+        return listStartsWith(rest1, rest2)
+
+# lexNext :: List[Char] -> Int -> Int -> List[Token] -> String -> Int -> List[Token]
+def lexInt(input : List[str], lineNr : int, charNr : int, tokens : List[Tokens.Token], number : str='', value : int=0) -> List[Tokens.Token]:
     if len(input) <= 0:
         if KC.integers[number[-1]] in range(1,10):
-            tokens.append(Tokens.Integer_Constant(lineNr, charNr-len(number), value + KC.integers[number[-1]]))
+            tokens.append(Tokens.Integer(lineNr, charNr-len(number), value + KC.integers[number[-1]]))
         else:
-            tokens.append(Tokens.Integer_Constant(lineNr, charNr-len(number), value))
+            tokens.append(Tokens.Integer(lineNr, charNr-len(number), value))
         return tokens
     char, *rest = input
-    print(char)
-    print(rest)
+    # ADD \n HANDLING!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     if char not in KC.integers:
         if KC.integers[number[-1]] in range(1,10):
             #print(KC.integers[number[-1]])
-            tokens.append(Tokens.Integer_Constant(lineNr, charNr-len(number), value + KC.integers[number[-1]]))
+            tokens.append(Tokens.Integer(lineNr, charNr-len(number), value + KC.integers[number[-1]]))
             return lexNext(input, lineNr, charNr, tokens)
         else:
             #print(value)
-            tokens.append(Tokens.Integer_Constant(lineNr, charNr-len(number), value))
+            tokens.append(Tokens.Integer(lineNr, charNr-len(number), value))
             return lexNext(input, lineNr, charNr, tokens)
     else:
         n = KC.integers[char]
@@ -35,7 +47,8 @@ def lexInt(input : str, lineNr : int, charNr : int, tokens : List[Tokens.Token],
             if len(number)<=0:
                 return lexInt(rest, lineNr, charNr + 1, tokens, number + char, value)
             elif KC.integers[number[-1]] in range(1,10):
-                tokens.append(Tokens.Integer_Constant(lineNr, charNr-len(number), value + KC.integers[number[-1]]))
+                print(f"[Lexer] Syntax Error: Number attached to other number without spacing at line {lineNr}, char {charNr}.")
+                tokens.append(Tokens.Integer(lineNr, charNr-len(number), value + KC.integers[number[-1]]))
                 return lexNext(input, lineNr, charNr, tokens)
             else:
                 return lexInt(rest, lineNr, charNr + 1, tokens, number + char, value)
@@ -46,27 +59,64 @@ def lexInt(input : str, lineNr : int, charNr : int, tokens : List[Tokens.Token],
                 return lexInt(rest, lineNr, charNr + 1, tokens, number + char, value + (n * KC.integers[number[-1]]))
             else:
                 return lexInt(rest, lineNr, charNr + 1, tokens, number + char, value + n)
-    
 
-# lexNext :: String -> Int -> Int -> List[Token] -> List[Token]
-def lexNext(input : str, lineNr : int, charNr : int, tokens : List[Tokens.Token]) -> List[Tokens.Token]:
+# lexNext :: List[Char] -> Int -> Int -> List[Token] -> String -> List[Token]
+def lexIdentifier(input : List[str], lineNr : int, charNr : int, tokens : List[Tokens.Token], name : str='') -> List[Tokens.Token]:
+    if len(input) <= 0:
+        print(f"[Lexer] Unexpected end of file at line {lineNr}, char {charNr}.")
+        return tokens
+    char, *rest = input
+    if char == '\n':
+        return lexIdentifier(rest, lineNr + 1, 1, tokens, name)
+    elif char == 'さ':
+        char2, *rest2 = rest
+        if char2 == 'ん':
+            tokens.append(Tokens.Identifier(lineNr, charNr-len(name), name + "さん"))
+            return lexNext(rest2, lineNr, charNr+2, tokens)
+    return lexIdentifier(rest, lineNr, charNr + 1, tokens, name + char)        
+
+# lexNext :: List[Char] -> Int -> Int -> List[Token] -> List[Token]
+def lexNext(input : List[str], lineNr : int, charNr : int, tokens : List[Tokens.Token]) -> List[Tokens.Token]:
     if len(input) <= 0:
         return tokens
     char, *rest = input
     if char == '\n':
         return lexNext(rest, lineNr + 1, 1, tokens)
+    elif char == ' ':
+        pass
     elif char == '〇':
-        tokens.append(Tokens.Integer_Constant(lineNr, charNr, 0))
-        return lexNext(rest, lineNr, charNr + 1, tokens)
+        tokens.append(Tokens.Integer(lineNr, charNr, 0))
     elif char in KC.integers:
-        #print(input)
         return lexInt(input, lineNr, charNr, tokens)
+    elif char == 'は':
+        tokens.append(Tokens.Assignment(lineNr, charNr))
+    elif listStartsWith(input, "です"):
+        tokens.append(Tokens.Assignment_End(lineNr, charNr))
+        return lexNext(rest[1:], lineNr, charNr + 2, tokens)
+    elif listStartsWith(input, "yesh"):
+        tokens.append(Tokens.Boolean(lineNr, charNr, True))
+        return lexNext(rest[3:], lineNr, charNr + 4, tokens)
+    elif listStartsWith(input, "nyet"):
+        tokens.append(Tokens.Boolean(lineNr, charNr, False))
+        return lexNext(rest[3:], lineNr, charNr + 4, tokens)
+    elif listStartsWith(input, "F "):
+        tokens.append(Tokens.Function_Definition(lineNr, charNr))
+        return lexNext(rest[1:], lineNr, charNr + 2, tokens)
+    elif listStartsWith(input, "OwO"):
+        tokens.append(Tokens.Code_Block_Start(lineNr, charNr))
+        return lexNext(rest[2:], lineNr, charNr + 3, tokens)
+    elif listStartsWith(input, "UwU"):
+        tokens.append(Tokens.Code_Block_End(lineNr, charNr))
+        return lexNext(rest[2:], lineNr, charNr + 3, tokens)
+    else:
+        print(input)
+        return lexIdentifier(input, lineNr, charNr, tokens)
     return lexNext(rest, lineNr, charNr + 1, tokens)
 
 # lex :: String -> List[Token]
 def lex(inputFilePath : str) -> List[Tokens.Token]:
     input = readFile(inputFilePath)
-    return lexNext(input, 1, 1, [])
+    return lexNext(list(input), 1, 1, [])
 
 for token in lex("input/test.sadge"):
     print(token)
--- source/Tokens.py
+++ source/Tokens.py
@@ -6,30 +6,76 @@ class Token:
 
     # __str__ :: None -> String
     def __str__(self) -> str:
-        return f"{self.__class__.__name__} at Line {self.lineNr}, Char {self.charNr}"
+        return f"{self.__class__.__name__} at Line {self.lineNr}, Char {self.charNr}."
 
-class Primitive_Type(Token):
-    # __init__ :: Int -> Int -> None
-    def __init__(self, lineNr : int, charNr : int) -> None:
+### UNUSED
+# class Primitive_Type(Token): 
+#     # __init__ :: Int -> Int -> None
+#     def __init__(self, lineNr : int, charNr : int) -> None:
+#         super().__init__(lineNr, charNr)
+#
+# class Integer_Type(Primitive_Type):
+#     # __init__ :: Int -> Int -> Int -> None
+#     def __init__(self, lineNr : int, charNr : int) -> None:
+#         super().__init__(lineNr, charNr)
+###
+
+class Boolean(Token):
+    # __innit__ :: Int -> Int -> Bool -> None
+    def __init__(self, lineNr : int, charNr : int, value : bool) -> None:
         super().__init__(lineNr, charNr)
+        self.value = value
 
-class Integer_Type(Primitive_Type):
-    # __init__ :: Int -> Int -> Int -> None
-    def __init__(self, lineNr : int, charNr : int) -> None:
-        super().__init__(lineNr, charNr)
+    # __str__ :: None -> String
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__} at Line {self.lineNr}, Char {self.charNr}, with value {self.value}."
 
-class Constant(Token):
-    # __innit__ :: Int -> Int -> None
-    def __init__(self, lineNr : int, charNr : int) -> None:
-        super().__init__(lineNr, charNr)
-        self.value = None
-
-class Integer_Constant(Constant):
-    # __innit__ :: Int -> Int -> None
+class Integer(Token):
+    # __innit__ :: Int -> Int -> Int -> None
     def __init__(self, lineNr : int, charNr : int, value : int) -> None:
         super().__init__(lineNr, charNr)
         self.value = value
 
     # __str__ :: None -> String
     def __str__(self) -> str:
-        return f"{self.__class__.__name__} at Line {self.lineNr}, Char {self.charNr}, with value {self.value}"
+        return f"{self.__class__.__name__} at Line {self.lineNr}, Char {self.charNr}, with value {self.value}."
+
+class Identifier(Token):
+    # __innit__ :: Int -> Int -> String -> None
+    def __init__(self, lineNr : int, charNr : int, name : str) -> None:
+        super().__init__(lineNr, charNr)
+        self.name = name
+
+    # __str__ :: None -> String
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__} at Line {self.lineNr}, Char {self.charNr}, with name {self.name}."
+
+class Operator(Token):
+    # __innit__ :: Int -> Int -> None
+    def __init__(self, lineNr : int, charNr : int) -> None:
+        super().__init__(lineNr, charNr)
+
+class Assignment(Operator):
+    # __innit__ :: Int -> Int -> None
+    def __init__(self, lineNr : int, charNr : int) -> None:
+        super().__init__(lineNr, charNr)
+
+class Assignment_End(Operator):
+    # __innit__ :: Int -> Int -> None
+    def __init__(self, lineNr : int, charNr : int) -> None:
+        super().__init__(lineNr, charNr)
+
+class Function_Definition(Token):
+    # __innit__ :: Int -> Int -> None
+    def __init__(self, lineNr : int, charNr : int) -> None:
+        super().__init__(lineNr, charNr)
+
+class Code_Block_Start(Token):
+    # __innit__ :: Int -> Int -> None
+    def __init__(self, lineNr : int, charNr : int) -> None:
+        super().__init__(lineNr, charNr)
+
+class Code_Block_End(Token):
+    # __innit__ :: Int -> Int -> None
+    def __init__(self, lineNr : int, charNr : int) -> None:
+        super().__init__(lineNr, charNr)
